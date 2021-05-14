ddf5aa5e33cfec18d9d05381fae918342b99e4d3
izbrannick <izbrannick@gmail.com>
2021-05-14 22:42:58 +0200

    Adding cy files

---
+++ cypress/integration/indextest.spec.js
@@ -0,0 +1,20 @@
+// indextest.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('index.html')
+    })
+  
+    // https://on.cypress.io/interacting-with-elements
+  
+    it('.type() - type into a DOM element', () => {
+    cy.wait(300);
+      cy.get('#ytplayer').click();
+    })
+  })
